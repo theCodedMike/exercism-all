9b6901541f179d9c6bd26a46e631255b7daf48b9
theCodedMike <lixiaofeng1115@outlook.com>
2025-10-22 14:14:50 +0800

    19. say 递归版本

--- exercism-cpp/say/say.cpp
+++ exercism-cpp/say/say.cpp
@@ -3,57 +3,20 @@
 #include <stdexcept>
 
 namespace say {
-    std::string special_treat(const std::int64_t num) {
-        switch (num)
-        {
-            case  1: return "one";
-            case  2: return "two";
-            case  3: return "three";
-            case  4: return "four";
-            case  5: return "five";
-            case  6: return "six";
-            case  7: return "seven";
-            case  8: return "eight";
-            case  9: return "nine";
-            case 10: return "ten";
-            case 11: return "eleven";
-            case 12: return "twelve";
-            case 13: return "thirteen";
-            case 14: return "fourteen";
-            case 15: return "fifteen";
-            case 16: return "sixteen";
-            case 17: return "seventeen";
-            case 18: return "eighteen";
-            case 19: return "nineteen";
-            case 20: return "twenty";
-            case 30: return "thirty";
-            case 40: return "forty";
-            case 50: return "fifty";
-            case 60: return "sixty";
-            case 70: return "seventy";
-            case 80: return "eighty";
-            case 90: return "ninety";
-            case 100: return "hundred";
-            default: return "";
-        }
-    }
+    const std::array<std::string, 21> numbers = {
+        "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen",
+        "twenty"
+    };
+    const std::array<std::string, 10> tenths = {
+        "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"
+    };
 
-    std::string in_english_1_to_999(std::int64_t num) {
-        if (num <= 0 || num > 999)
-            throw std::domain_error("Number out of range");
+    std::string process_larger(const std::int64_t num, const int64_t base, const std::string& base_in_english) {
+        auto res = in_english(num / base) + base_in_english;
 
-        std::string res;
-        if (num >= 100) {
-            res = special_treat(num / 100) + " hundred ";
-            num %= 100;
-        }
-        if (num > 20) {
-            res += special_treat(num - num % 10);
-            num %= 10;
-            res += num != 0 ? '-' : ' ';
-        }
-        if (num > 0)
-            res += special_treat(num) + ' ';
+        if (num % base != 0)
+            res += " " + in_english(num % base);
 
         return res;
     }
@@ -62,27 +25,23 @@ namespace say {
         if (num < 0 || num > 999999999999)
             throw std::domain_error("Number out of range");
 
-        if (num == 0)
-            return "zero";
-
-        constexpr std::array<std::pair<std::int64_t, const char *>, 4> number_sections = {{
-                {1'000'000'000, "billion "},
-                {1'000'000, "million "},
-                {1'000, "thousand "},
-                {1, ""}
-
-        }};
         std::string res;
 
-        for (const auto &[base, base_english]: number_sections) {
-            if (num >= base) {
-                res += in_english_1_to_999(num / base);
-                res += base_english;
-                num %= base;
-            }
-        }
+        if (num <= 20)
+            res = numbers[num];
+        else if (num < 100)
+            res = tenths[num / 10] + (num % 10 == 0 ? "" : "-" + numbers[num % 10]);
+        else if (num < 1000)
+            res = process_larger(num, 100, " hundred");
+        else if (num < 1000000)
+            res = process_larger(num, 1000, " thousand");
+        else if (num < 1000000000)
+            res = process_larger(num, 1000000, " million");
+        else if (num < 1000000000000)
+            res = process_larger(num, 1000000000, " billion");
+        else
+            throw std::domain_error("Number out of range");
 
-        res.pop_back();
         return res;
     }
 } // namespace say
